81dccb3429935c3ffe77635aa01f49f3b219d51a
Johannes Brix <johannes.brix7@gmail.com>
2025-04-21 09:42:11 +0200

    Setup connection for mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,19 @@
 // const express = require("express");
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+// request and response
+app.get("/", (req, res) => {
+    res.send("Server is ready.");
+});
+
+// 5000 is the port
+app.listen(5000, () => {
+    connectDB(); // connect Database 
+    console.log("Server started at http://localhost:5000");
+});
